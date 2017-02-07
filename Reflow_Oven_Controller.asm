23c9ca9412bd51b8221b88ba204025664b694ff9
Eric Chen <Eric Chen>
2017-02-07 10:01:56 -0800

    First commit

---
+++ Reflow_Oven_Controller.asm
@@ -0,0 +1,88 @@
+$NOLIST
+$MODLP52
+$LIST
+
+
+
+;-----------------------------------------------
+; Constants
+CLK          EQU 22118400 ; Microcontroller system crystal frequency in Hz
+TIMER0_RATE   EQU 4096     ; 2048Hz squarewave (peak amplitude of CEM-1203 speaker)
+TIMER0_RELOAD EQU ((65536-(CLK/TIMER0_RATE)))
+TIMER2_RATE   EQU 1000     ; 1000Hz, for a timer tick of 1ms
+TIMER2_RELOAD EQU ((65536-(CLK/TIMER2_RATE)))
+
+
+
+
+
+;-----------------------------------------------
+; Pin assignment
+BOOT_BUTTON   equ P4.5
+SOUND_OUT     equ P3.7
+UPDOWN        equ P0.0
+SHIFT_BUTTON  equ P0.7
+MY_VARIABLE_BUTTON  equ P0.4
+
+
+
+
+
+;----------------------------------------------
+; Reset vector, the following are vector addresses for ISRs
+org 0x0000
+    ljmp main
+
+; External interrupt 0 vector (not used in this code)
+org 0x0003
+	reti
+
+; Timer/Counter 0 overflow interrupt vector
+org 0x000B
+	ljmp Timer0_ISR
+
+; External interrupt 1 vector (not used in this code)
+org 0x0013
+	reti
+
+; Timer/Counter 1 overflow interrupt vector (not used in this code)
+org 0x001B
+	reti
+
+; Serial port receive/transmit interrupt vector (not used in this code)
+org 0x0023 
+	reti
+	
+; Timer/Counter 2 overflow interrupt vector
+org 0x002B
+	ljmp Timer2_ISR
+
+	
+
+	
+
+;-----------------------------------------------	
+; In the 8051 we can define direct access variables starting at location 0x30 up to location 0x7F
+dseg at 0x30
+
+
+
+
+
+
+;-----------------------------------------------
+; In the 8051 we have variables that are 1-bit in size.  We can use the setb, clr, jb, and jnb
+; instructions with these variables.  This is how you define a 1-bit variable:
+bseg
+half_seconds_flag: dbit 1 ; Set to one in the ISR every time 500 ms had passed
+
+
+
+
+
+
+
+
+
+
+
